5815978b7709d6b24751c53f45092f459898f7fb
Ismail <caspian-seagull@users.noreply.github.com>
2017-01-01 17:11:20 +0300

    Update mover.js

--- mover.js
+++ mover.js
@@ -1,46 +1,40 @@
 (function initMovers() {
-  let movers = [].slice.call(document.querySelectorAll(".js-mover"));
-  
-  function getPositionRatio(event) {
-    let Mx = event.clientX;
-    let My = event.clientY;
-
-    let kx = (Mx - CENTER.x) / CENTER.x;
-    let ky = (My - CENTER.y) / CENTER.y;
-
-    return {
-      x: parseFloat( kx.toFixed(3) ),
-      y: parseFloat( ky.toFixed(3) )
-    }
-  }
-  
-  function setMoverStyle(mover, ratio) {
-    let data = mover.dataset.offset.split(" ");
-
-    let params = {
-      dx: data[0] * ratio.x,
-      dy: data[1] * ratio.y,
-      rx: data[2] * ratio.x,
-      ry: data[3] * ratio.y 
+    var movers = [].slice.call(document.querySelectorAll('.js-mover'));
+    var getPositionRatio = function getPositionRatio(event) {
+        var Mx = event.clientX;
+        var My = event.clientY;
+        var kx = (Mx - CENTER.x) / CENTER.x;
+        var ky = (My - CENTER.y) / CENTER.y;
+        return {
+            x: parseFloat(kx.toFixed(3)),
+            y: parseFloat(ky.toFixed(3))
+        };
     };
-
-    let transformString = "perspective(1000px) ";
-
-    params.dx !== 0 ? transformString += `translateX(${params.dx}px) ` : null;
-    params.dy !== 0 ? transformString += `translateY(${params.dy}px) ` : null;
-    params.rx !== 0 ? transformString += `rotateX(${params.ry}deg) ` : null;
-    params.ry !== 0 ? transformString += `rotateY(${params.rx}deg) ` : null;
-
-    if (TweenMax) {
-       TweenMax.to(mover, 0.8, {css:{transform: transformString}})
-    } else {
-       mover.style.transform = transformString;
-       mover.style.webkitTransform = transformString;
-    }
-  }
-  
-  document.body.addEventListener("mousemove", (e) => {
-    let mouseRatio = getPositionRatio(e);
-    movers.forEach( mover => setMoverStyle(mover, mouseRatio) );
-  });
-})();
+    var setMoverStyle = function setMoverStyle(mover, ratio) {
+        var data = mover.dataset.offset.split(' ');
+        var params = {
+            dx: data[0] * ratio.x,
+            dy: data[1] * ratio.y,
+            rx: data[2] * ratio.x,
+            ry: data[3] * ratio.y
+        };
+        var transformString = 'perspective(1000px) ';
+        params.dx !== 0 ? transformString += 'translateX(' + params.dx + 'px) ' : null;
+        params.dy !== 0 ? transformString += 'translateY(' + params.dy + 'px) ' : null;
+        params.rx !== 0 ? transformString += 'rotateX(' + params.ry + 'deg) ' : null;
+        params.ry !== 0 ? transformString += 'rotateY(' + params.rx + 'deg) ' : null;
+        if (TweenMax) {
+            TweenMax.to(mover, 0.8, { css: { transform: transformString } });
+        } else {
+            mover.style.transform = transformString;
+            mover.style.webkitTransform = transformString;
+        }
+    };
+    var _this = this;
+    document.body.addEventListener('mousemove', function (e) {
+        var mouseRatio = getPositionRatio(e);
+        movers.forEach(function (mover) {
+            return setMoverStyle(mover, mouseRatio);
+        });
+    });
+}());
