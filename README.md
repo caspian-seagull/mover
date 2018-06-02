8aa8ef3633ea369aee7e0d963ddf92bf286c90f8
caspianseagull <caspian.seagull@gmail.com>
2018-06-02 23:52:59 +0300

    Switched to tweenilte, less code, speed parameter

--- README.md
+++ README.md
@@ -5,27 +5,29 @@ Moves and rotates elements in window depend on cursor's position
 
 ## usage
 ```HTML
-<script src="move.min.js"></script>
+<!-- Use gsap TweenLite with css plugin for smooth movement -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenLite.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/plugins/CSSPlugin.min.js"></script>
 
-<!-- Use gsap TweenMax for smooth movement -->
-<script src="TweenMax.min.js"></script>
+<script src="mover.min.js"></script>
 
-<span class="js-mover" data-offset="30 20 5 5">
+<span class="js-mover" data-offset="30 20 5 5 0.8">
   Content
 <span>
 ```
 
 ## data-offset value format
 ```
-"dx dy rx ry"
+"dx dy rx ry speed"
 ```
 
 * dx - limit offset on x axis
 * dy - limit offset on y axis
 * rx - limit x-rotation value
 * ry - limit y-rotation value
+* sp - speed of easeing (default 0.8)
 
-Use 0 to disable offset
+Use 0 to disable offset or easing
 ```
-data-offset="20 0 0 0"
+data-offset="20 0 0 0 0"
 ```
--- mover.js
+++ mover.js
@@ -1,49 +1,47 @@
 (function initMovers() {
-    var movers = [].slice.call(document.querySelectorAll('.js-mover'));
-    var getPositionRatio = function getPositionRatio(event) {
-        var W_WIDTH  = window.innerWidth;
-        var W_HEIGHT = window.innerHeight;
+  var movers = [].slice.call(document.querySelectorAll('.js-mover'));
+  var TweenLite = window.TweenLite;
 
-        var CENTER = {x: W_WIDTH/2, y: W_HEIGHT/2};
-        
-        var Mx = event.clientX;
-        var My = event.clientY;
-        var kx = (Mx - CENTER.x) / CENTER.x;
-        var ky = (My - CENTER.y) / CENTER.y;
-        return {
-            x: parseFloat(kx.toFixed(3)),
-            y: parseFloat(ky.toFixed(3))
-        };
+  var getPositionRatio = function getPositionRatio(event) {
+    var CENTER = {x: window.innerWidth/2, y: window.innerHeight/2};
+    var kx = (event.clientX - CENTER.x) / CENTER.x;
+    var ky = (event.clientY - CENTER.y) / CENTER.y;
+    return {
+      x: parseFloat(kx.toFixed(3)),
+      y: parseFloat(ky.toFixed(3))
     };
-    var setMoverStyle = function setMoverStyle(mover, ratio) {
-        var data = mover.dataset.offset.split(' ');
-        var params = {
-            dx: data[0] * ratio.x,
-            dy: data[1] * ratio.y,
-            rx: data[2] * ratio.x,
-            ry: data[3] * ratio.y
-        };
-        var transformString = 'perspective(1000px) ';
-        params.dx !== 0 ? transformString += 'translateX(' + params.dx + 'px) ' : null;
-        params.dy !== 0 ? transformString += 'translateY(' + params.dy + 'px) ' : null;
-        params.rx !== 0 ? transformString += 'rotateX(' + params.ry + 'deg) ' : null;
-        params.ry !== 0 ? transformString += 'rotateY(' + params.rx + 'deg) ' : null;
-        
-        window.requestAnimationFrame(function() {
-            if (TweenMax) {
-              TweenMax.to(mover, 0.8, { css: { transform: transformString } });
-            } else {
-              mover.style.transform = transformString;
-              mover.style.webkitTransform = transformString;
-            }
-        });
+  };
+  
+  var setMoverStyle = function setMoverStyle(mover, ratio) {
+    var data = mover.dataset.offset.split(' ');  
+    var speed = data[4] || 0.8;
+    var params = {
+      dx: data[0] * ratio.x,
+      dy: data[1] * ratio.y,
+      rx: data[2] * ratio.x,
+      ry: data[3] * ratio.y
     };
-    var _this = this;
-    window.addEventListener('mousemove', function (e) {
-        var mouseRatio = getPositionRatio(e);
-      
-        movers.forEach(function (mover) {
-            return setMoverStyle(mover, mouseRatio);
-        });
+    
+    var transformString = 'perspective(1000px) ';
+    params.dx !== 0 ? transformString += 'translateX(' + params.dx + 'px) ' : null;
+    params.dy !== 0 ? transformString += 'translateY(' + params.dy + 'px) ' : null;
+    params.rx !== 0 ? transformString += 'rotateX(' + params.ry + 'deg) ' : null;
+    params.ry !== 0 ? transformString += 'rotateY(' + params.rx + 'deg) ' : null;
+
+    window.requestAnimationFrame(function() {
+      if (TweenLite) {
+        TweenLite.to(mover, speed, { css: { transform: transformString } });
+      } else {
+        mover.style.transform = transformString;
+        mover.style.webkitTransform = transformString;        
+      }
     });
+  };
+  
+  window.addEventListener('mousemove', function (e) {
+    var mouseRatio = getPositionRatio(e);
+    movers.forEach(function(mover) {
+      return setMoverStyle(mover, mouseRatio);
+    });
+  });
 }());
